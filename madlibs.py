1bbeb6d7a2749e4ae8082afd4dcbaae81b83ca15
Mashen_Taterz <ty_beach@live.com>
2023-03-17 17:26:17 -0700

    First Commit

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+###MADLIBS###
+
+noun = input("Noun: ")
+adj = input("Adjective: ")
+verb = input("Verb: ")
+adverb = input("Adverb: ")
+
+madlib = f"""I love prgamming because it is {adj}. 
+I was inspired by {noun} to start my journey. 
+A good way to remember what I learn is to {adverb} write down notes. 
+When I am not busy practicing I like to {verb}."""
+
+print(madlib)
